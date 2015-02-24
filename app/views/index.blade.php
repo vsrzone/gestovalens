cde0030f7c0f26988d83048cd521cd0a5c113a22
vsrzone <vikumsri@gmail.com>
2015-02-25 01:29:25 +0530

    basic Layout done

---
+++ app/views/index.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Gesto Valens</title>
+	<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="{{ url() }}/css/style.css">
+</head>
+<body>
+	<div id="page-wrapper">
+		<div id="item-wrapper">
+		</div>
+		<div id="main-wrapper">
+			<div id="deisgn-area"></div>
+			<div id="editing-menu">
+				<div id="color-selection-menu">
+					<div id="selection-menu-icons">
+						<div class="color-tab-icon" id="tshirt-color-tab"></div>
+						<div class="color-tab-icon" id="design-color-tab"></div>
+					</div>
+					<div class="color-selection-row">
+						<div class="color-block"></div>
+						<div class="color-block color-block-margin"></div>
+						<div class="color-block color-block-margin"></div>
+						<div class="color-block color-block-margin"></div>
+						<div class="color-block color-block-margin"></div>
+					</div>
+					<div class="color-selection-row">
+						<div class="color-block"></div>
+						<div class="color-block color-block-margin"></div>
+						<div class="color-block color-block-margin"></div>
+						<div class="color-block color-block-margin"></div>
+						<div class="color-block color-block-margin"></div>
+					</div>
+					<div class="sample-designs-row">
+						<div class="sample-design"></div>
+						<div class="sample-design sample-design-margin"></div>
+					</div>
+					<div class="sample-designs-row">
+						<div class="sample-design"></div>
+						<div class="sample-design sample-design-margin"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<script type="text/javascript" src="{{ url() }}/js/isotope.pkgd.min.js"></script>
+</html>
