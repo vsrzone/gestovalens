39e0b6506edd7fa777ef0a5960569830a6011927
vsrzone <vikumsri@gmail.com>
2015-02-26 16:51:44 +0530

    tshirt color change and artwork add functions done

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -13,39 +13,41 @@
 		<div id="item-wrapper">
 		</div>
 		<div id="main-wrapper">
-			<div id="deisgn-area"></div>
+			<div id="deisgn-area">
+				<canvas id="design-canvas"></canvas>
+			</div>
 			<div id="editing-menu">
-				<div id="color-selection-menu">
-					<div id="selection-menu-icons">
-						<div class="color-tab-icon" id="tshirt-color-tab"></div>
-						<div class="color-tab-icon" id="design-color-tab"></div>
+				<div id="editing-fixed">	
+					<div id="color-selection-menu">
+						<div id="selection-menu-icons">
+							<div class="color-tab-icon" id="tshirt-color-tab"></div>
+							<div class="color-tab-icon" id="design-color-tab"></div>
+						</div>
+						<div id="color-wrapper">
+						</div>
+						<div id="artworks-wrapper">
+						</div>
+						<div id="more-designs">more designs</div>
 					</div>
-					<div class="color-selection-row">
-						<div class="color-block"></div>
-						<div class="color-block color-block-margin"></div>
-						<div class="color-block color-block-margin"></div>
-						<div class="color-block color-block-margin"></div>
-						<div class="color-block color-block-margin"></div>
-					</div>
-					<div class="color-selection-row">
-						<div class="color-block"></div>
-						<div class="color-block color-block-margin"></div>
-						<div class="color-block color-block-margin"></div>
-						<div class="color-block color-block-margin"></div>
-						<div class="color-block color-block-margin"></div>
-					</div>
-					<div class="sample-designs-row">
-						<div class="sample-design"></div>
-						<div class="sample-design sample-design-margin"></div>
-					</div>
-					<div class="sample-designs-row">
-						<div class="sample-design"></div>
-						<div class="sample-design sample-design-margin"></div>
+					<div id="footer">
+						<div id="social-icons-row">
+							<div class="social-icon" id="fb-icon"></div>
+							<div class="social-icon" id="twitter-icon"></div>
+						</div>
+						<div id="copyrights">designed by <span style="color: #858585">ingens</span></div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </body>
+<script type="text/javascript">
+	var http_path = '{{ url() }}';
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script type="text/javascript" src="{{ url() }}/js/PxLoader.js"></script>
+<script type="text/javascript" src="{{ url() }}/js/PxLoaderImage.js"></script>
 <script type="text/javascript" src="{{ url() }}/js/isotope.pkgd.min.js"></script>
+<script type="text/javascript" src="{{ url() }}/js/libs.js"></script>
+
 </html>
---
+++ public/js/libs.js
@@ -0,0 +1,223 @@
+var colors = {'t-blue':'#039ddd', 
+			't-ash':'#8f8f90', 
+			't-black':'#222222', 
+			't-yellow':'#eccf47', 
+			't-darkgreen':'#3c5542', 
+			't-red':'#f53a4b', 
+			't-green':'#0fc497',
+			't-white':'#eeeeee',
+			't-darkblue':'#103684',
+			't-pink':'#ca34d3'
+		};
+
+// canvas variables
+var designCanvas = document.getElementById('design-canvas');
+var designContext = designCanvas.getContext("2d");
+var design_bg;
+var design_area;
+
+$( document ).ready(function(){
+	genarateColorBoxes();
+	initDesignCanvas();
+
+	$(document).on('click','.color-block', function(e){
+		var color = e.target.id;
+		addTshirt(colors[color]);
+	});
+
+	$(document).on('click','.sample-design', function(e){
+		var artwork = $(e.target).data('artwork-id');
+		addArtwok(artwork);
+	});
+
+	generateArtworks();
+});
+
+// function for color boxes
+
+function genarateColorBoxes(){
+	var container = $('#color-wrapper');
+	var count = 0;
+	var currentRow;
+
+	for(var color in colors){
+		if (count % 5 == 0) {
+			currentRow = $('<div class="color-selection-row">');
+			container.append(currentRow);
+		};
+
+		var box = $('<div class="color-block"></div>');
+		box.css('background-color', colors[color]);
+		box.attr('id', color);
+
+		currentRow.append(box);
+
+		count++;
+	}
+}
+
+// main initialisation for the design floor
+
+function initDesignCanvas(){
+	designCanvas.width = $(designCanvas.parentNode).innerWidth();
+	designCanvas.height = $(designCanvas.parentNode).innerHeight();
+
+	var loader = new PxLoader();
+	design_bg = loader.addImage(http_path+'/images/design_bg.png');
+	design_area = loader.addImage(http_path+'/images/design_area.png');
+
+	loader.addCompletionListener(function() { 
+		drawImage(design_bg, designCanvas, designContext);
+		drawImage(design_area, designCanvas, designContext);
+
+		var loader = new PxLoader();
+		for (var i = 0; i < artworks.length; i++) {
+			artworks[i].image = loader.addImage(http_path+artworks[i]['url']);
+		};
+
+		loader.addCompletionListener(function(){
+
+		});
+
+		loader.start();
+
+	});
+
+	loader.start();
+}
+
+function drawImage(image, canvas, ctx, opacity){
+	if (opacity == undefined) {
+		opacity = 1;
+	}
+	var image_width;
+	var image_height;
+
+	ctx.globalAlpha = opacity;
+	image_height = canvas.height;
+	image_width = image.width / image.height * canvas.height;
+
+	var x_pos  = (canvas.width - image_width) / 2;
+
+	ctx.drawImage(image, x_pos, 0, image_width, image_height);
+}
+
+function addTshirt(color){
+	colorCanvas = document.createElement('canvas');
+	colorCtx = colorCanvas.getContext('2d');
+
+	colorCanvas.width = designCanvas.width;
+	colorCanvas.height = designCanvas.height;
+
+	drawImage(design_area, colorCanvas, colorCtx);
+	var map = colorCtx.getImageData( 0, 0, colorCanvas.width, colorCanvas.height);
+	var imageData = map.data;
+	
+	// convert to grayscale
+
+	for (var i = 0; i < imageData.length; i+=4) {
+		var r = imageData[i];
+		var g = imageData[i+1];
+		var b = imageData[i+2];
+
+		var avg = (r+g+b)/3;
+
+		imageData[i] = imageData[i+1] = imageData[i+2] = avg; 
+	};
+	
+	map.data = imageData;
+
+	colorCtx.putImageData(map,0,0);
+
+	// overlay with colors
+	
+	colorCtx.globalCompositeOperation = 'overlay';
+	//colorCtx.globalAlpha = 0.5;
+	colorCtx.fillStyle = color;
+	colorCtx.fillRect(0,0,colorCanvas.width,colorCanvas.height);
+
+	colorCtx.globalCompositeOperation = 'destination-in';
+	drawImage(design_area, colorCanvas, colorCtx);
+
+	colorTransition(colorCanvas, designCanvas, designContext, 0);
+}
+
+function colorTransition(image, canvas, ctx, opacity){
+	window.requestAnimationFrame(function(){
+		drawImage(image, canvas, ctx, opacity);
+		drawImage(artworkCanvas, canvas, ctx);
+		opacity += 0.05;
+		if(opacity <= 1){
+			colorTransition(image, canvas, ctx, opacity);
+		}
+	});
+}
+
+// creating design objects
+
+var artworks = [
+			{
+				id : 'i_am_danger',
+				url : '/images/designs/i_am_danger.png'
+			},
+			{
+				id : 'icc_cwc', 
+				url : '/images/designs/icc_cwc_sri_lanka.png'
+			},
+			{
+				id : 'sons_of_anarchy', 
+				url : '/images/designs/sons_of_anarchy.png'
+			},
+			{
+				id : 'winter_is_coming', 
+				url : '/images/designs/winter_is_coming.png'
+			}
+	];
+
+
+
+function generateArtworks(){
+	var container = $('#artworks-wrapper');
+	var count = 0;
+	var currentRow;
+
+	for (var i = 0; i < artworks.length; i++) {
+		 if(count % 2 == 0){
+		 	currentRow = $('<div class="sample-designs-row">');
+		 	container.append(currentRow);
+		 }
+
+		 var box = $('<div class="sample-design"><div class="artwork-wrapper"></div></div>');
+		 box.children('.artwork-wrapper').css('background-image', 'url('+http_path+artworks[i]['url']+')');
+		 box.children('.artwork-wrapper').attr('id', artworks[i]['id']);
+		 box.children('.artwork-wrapper').data('artwork-id', i);
+
+		 currentRow.append(box);
+
+		 count++;
+	};
+}
+
+// add artwork to canvas
+
+function addArtwok(artwork){
+	artworkCanvas = document.createElement('canvas');
+	artworkCtx = artworkCanvas.getContext('2d');
+
+	artworkCanvas.width = designCanvas.width;
+	artworkCanvas.height = designCanvas.height;
+
+	var artwork_object = artworks[artwork].image;
+		
+	var image_height = artworkCanvas.height/10*4;
+	var image_width = artwork_object.width/artwork_object.height*image_height;
+
+	var x_pos = (artworkCanvas.width-image_width)/2;
+	var y_pos = artworkCanvas.height/7;
+
+
+	artworkCtx.drawImage(artwork_object, x_pos, y_pos, image_width, image_height);
+
+	drawImage(artworkCanvas, designCanvas, designContext);
+
+}
